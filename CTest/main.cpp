a9fcfc133fcd4e788a350ee9cff3cd1d58c131cf
ourfor <ourfor@qq.com>
2022-12-28 09:07:23 +0800

    Initial Commit

---
+++ CTest/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CTest
+//
+//  Created by 梁甜 on 2022/12/28.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
